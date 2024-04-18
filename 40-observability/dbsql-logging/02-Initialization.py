a17c98af1c2b6f2fefe960fe70b2cf1fd7945b26
shyamraodb <129457417+shyamraodb@users.noreply.github.com>
2024-04-18 13:59:12 -0700

    Delete 40-observability directory

--- 40-observability/dbsql-logging/02-Initialization.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-# MAGIC %run ./00-Config
-
-# COMMAND ----------
-
-spark.sql(f'CREATE DATABASE IF NOT EXISTS {DATABASE_NAME}') 
-# optional: add location 
-# spark.sql(f'CREATE DATABASE IF NOT EXISTS {DATABASE_NAME} LOCATION {DATABASE_LOCATION}') 
